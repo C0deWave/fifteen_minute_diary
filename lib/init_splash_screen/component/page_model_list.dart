32e41bf149e5c9f8cdde08212571f256865a5afc
CodeWave <jamg123123@naver.com>
2021-12-14 00:05:33 +0900

    Style: InitSplashScreen UI및 내용 개선
    벡터이미지인 svg이미지를 이용
    - flutter_svg 패키지를 이용
    이미지 UI는 undraw 사이트에서 수집
    - 색상코드 #29A658 사용

--- lib/init_splash_screen/component/page_model_list.dart
+++ lib/init_splash_screen/component/page_model_list.dart
@@ -1,28 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 var listPagesViewModel = <PageViewModel>[
   PageViewModel(
     title: "하루의 1%를 투자하세요",
-    body: "15Minute Diary는 15분의 시간제한을 두고 쓰는 일기장 앱입니다.",
+    body: "15Minute Diary는 15분의 \n시간제한이 있는 일기장 앱입니다.",
     image: Padding(
       padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
       child: Center(
-        child: Image.network(
-          "https://image.yes24.com/images/00_Event/2021/yesPresent/0623Miracle/img001.jpg",
-          fit: BoxFit.fill,
-        ),
+        child: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SvgPicture.asset(
+              "asset/image/init_splash_screen/init1page_studing.svg"),
+        )
+            // Image.asset(
+            //   "/asset/image/init_splash_screen/init1page_studing.svg",
+            //   fit: BoxFit.fill,
+            // ),
+            ),
       ),
     ),
   ),
   PageViewModel(
-    title: "Title of first page",
-    body:
-        "Here you can write the description of the page, to explain someting...",
+    title: "당신의 지난 날을 되돌아 보세요",
+    body: "매일 1%의 시간동안 기록하지만 \n일기장에는 당신의 100%가 있습니다.",
     image: Center(
-      child: Image.network(
-          "https://lh3.googleusercontent.com/proxy/3tcAIoY9mkIZ_H9UZyStrTcLtTPkxu-C4pyed7JrLOmnjm2lTZu2xBTKTIH0osztwpldpp1ytW5rulv24aSY42gjGKKPpDyKZqOTw89CspWi-D33O_Q",
-          height: 400.0),
+      child: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SvgPicture.asset(
+            "asset/image/init_splash_screen/init2page_camping.svg"),
+      )
+          // Image.network(
+          //     "/asset/image/init_splash_screen/init2page_camping.svg",
+          //     height: 400.0),
+          ),
+    ),
+  ),
+  PageViewModel(
+    title: "15분만 쓸 수 있음에 주의하세요!",
+    body: "역설적이게도 일기를 더 잘 쓰게 됩니다.\n15분이 지나면 강제로 일기가 작성됩니다.",
+    image: Center(
+      child: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SvgPicture.asset(
+            "asset/image/init_splash_screen/init3page_notify.svg"),
+      )
+          // Image.network(
+          //     "/asset/image/init_splash_screen/init3page_notify.svg",
+          //     height: 400.0),
+          ),
     ),
   ),
 ];
