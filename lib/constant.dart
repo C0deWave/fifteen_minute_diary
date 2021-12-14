c92a661735683f5c31e6a57c4cb0becab0d446a2
CodeWave <jamg123123@naver.com>
2021-12-15 00:49:54 +0900

    Style: 피드백을 받아 initSplashScreen의 내용 및 포맷 변경
    이미지 shape를 전부 rectangle로 변경
    기존의 원형이미지 교체
    하지만 constant 부분에서 언제든지 원형으로 쉽게 교체 가능하게 만듬
    svg_provider를 설치해서 svg vector파일을 provider화

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,2 +1,8 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
 const kDebugMode = true;
-const isViewSplashView_Key = "testIsViewSplashView14";
+const isViewSplashView_Key = "testIsViewSplashView16";
+// const initSplashBackgroundImageColor = Color.fromARGB(255, 173, 179, 216);
+const initSplashBackgroundImageColor = Colors.white12;
+const initSplashImageShape = BoxShape.rectangle;
--- lib/init_splash_screen/component/page_model_list.dart
+++ lib/init_splash_screen/component/page_model_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+
+import '../../constant.dart';
 
 var listPagesViewModel = <PageViewModel>[
   PageViewModel(
@@ -10,49 +12,47 @@ var listPagesViewModel = <PageViewModel>[
       padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
       child: Center(
         child: SafeArea(
-            child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SvgPicture.asset(
-              "asset/image/init_splash_screen/init1page_studing.svg"),
-        )
-            // Image.asset(
-            //   "/asset/image/init_splash_screen/init1page_studing.svg",
-            //   fit: BoxFit.fill,
-            // ),
-            ),
+            child: Container(
+          decoration: BoxDecoration(
+              color: initSplashBackgroundImageColor,
+              shape: initSplashImageShape,
+              image: DecorationImage(
+                image: Svg(
+                    "asset/image/init_splash_screen/init1page_notebook.svg"),
+              )),
+        )),
       ),
     ),
   ),
   PageViewModel(
-    title: "당신의 지난 날을 되돌아 보세요",
-    body: "매일 1%의 시간동안 기록하지만 \n일기장에는 당신의 100%가 있습니다.",
+    title: "15분만 쓸 수 있음에 주의하세요!",
+    body: "15분은 하루의 1%정도 입니다.\n15분이 지나면 강제로 일기가 저장됩니다.",
     image: Center(
       child: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SvgPicture.asset(
-            "asset/image/init_splash_screen/init2page_camping.svg"),
-      )
-          // Image.network(
-          //     "/asset/image/init_splash_screen/init2page_camping.svg",
-          //     height: 400.0),
-          ),
+          child: Container(
+        decoration: BoxDecoration(
+            shape: initSplashImageShape,
+            color: initSplashBackgroundImageColor,
+            image: DecorationImage(
+              image: Svg("asset/image/init_splash_screen/init3page_notify.svg"),
+            )),
+      )),
     ),
   ),
   PageViewModel(
-    title: "15분만 쓸 수 있음에 주의하세요!",
-    body: "역설적이게도 일기를 더 잘 쓰게 됩니다.\n15분이 지나면 강제로 일기가 작성됩니다.",
+    title: "당신의 지난 날을 되돌아 보세요",
+    body: "매일 1%의 시간동안 기록하지만 \n당신은 100%를 기억할 수 있습니다.",
     image: Center(
       child: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SvgPicture.asset(
-            "asset/image/init_splash_screen/init3page_notify.svg"),
-      )
-          // Image.network(
-          //     "/asset/image/init_splash_screen/init3page_notify.svg",
-          //     height: 400.0),
-          ),
+          child: Container(
+        decoration: BoxDecoration(
+            color: initSplashBackgroundImageColor,
+            shape: initSplashImageShape,
+            image: DecorationImage(
+              image:
+                  Svg("asset/image/init_splash_screen/init2page_camping.svg"),
+            )),
+      )),
     ),
   ),
 ];
