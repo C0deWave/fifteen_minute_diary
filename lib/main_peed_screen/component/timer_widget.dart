5fee7f6c6d9a8aeaa485eeff2c70e78f6422b322
CodeWave <jamg123123@naver.com>
2021-12-23 22:59:24 +0900

    Fix: TimerWidget Hero이동시 Render공간 부족 에러 해결
    타이머의 크기를 고정값으로 변경해서 해결
    파일명 알맞게 수정

---
+++ lib/main_peed_screen/component/timer_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../controller/timer_controller.dart';
+
+class TimerWidget extends StatelessWidget {
+  const TimerWidget({Key? key, required this.callback}) : super(key: key);
+  final Function callback;
+  @override
+  Widget build(BuildContext context) {
+    final timerController = Get.put(TimerController());
+    return GestureDetector(
+      onTap: () {
+        callback();
+      },
+      child: Container(
+        height: 100,
+        // height: Get.height / 7,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(11),
+            color: Colors.grey.shade300),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            //TODO 타이머 기능 추가하기
+            // 메인 화면에서는 가만히 있다가
+            // 일기 화면으로 넘어가면 카운트 시작
+            children: [
+              Obx(() {
+                return Text(
+                  "${timerController.time}",
+                  style: const TextStyle(
+                      fontSize: 60,
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      decoration: TextDecoration.none),
+                );
+              }),
+              Obx(() {
+                return Text(
+                  "${timerController.subtext}",
+                  style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      decoration: TextDecoration.none),
+                );
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
