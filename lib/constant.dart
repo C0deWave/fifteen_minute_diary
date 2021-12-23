90eeab079b51b46a59009049992618f9d34fbc6b
CodeWave <jamg123123@naver.com>
2021-12-23 22:57:57 +0900

    Feat: 글쓰기 화면에서 타이머 누를시 나가기 alert 띄움
    alert내용은 constant.dart에 추가
    글쓰기 화면에서 back swipe를 이용한 뒤로가기 불가능 하게 설정
     - Widget의 소멸자를 찾지 못해서 back swipe를 이용한 pop을 할시 타이머 멈추기가 쉽지 않음

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 
 const kDebugMode = true;
@@ -10,3 +9,9 @@ const initSplashImageShape = BoxShape.rectangle;
 const caturl =
     "https://s3.ap-northeast-2.amazonaws.com/elasticbeanstalk-ap-northeast-2-176213403491/media/magazine_img/magazine_280/5-3-썸네일.jpg";
 const k_timer_herotag = "herowidget";
+
+const k_alert_title = "일기 종료";
+const k_alert_content = "일기를 HH:MM초동안 쓰셨어요.\n다 쓰셨나요?";
+const k_alert_yes = "넵";
+const k_alert_no = "아니요";
+const k_alert_cancle = "다음에 쓸래요!";
--- lib/write_diary_screen/write_diary_screen.dart
+++ lib/write_diary_screen/write_diary_screen.dart
@@ -1,67 +1,128 @@
+import 'dart:io';
+
 import 'package:fifteen_minute_diary/constant.dart';
-import 'package:fifteen_minute_diary/main_peed_screen/component/TimerWidget.dart';
+import 'package:fifteen_minute_diary/main_peed_screen/component/timer_widget.dart';
+import 'package:fifteen_minute_diary/main_peed_screen/main_peed_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get/route_manager.dart';
+
+import '../controller/timer_controller.dart';
 
 class WriteDiaryScreen extends StatelessWidget {
   const WriteDiaryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Get.back();
-        },
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(height: 4),
-            Hero(
-                tag: k_timer_herotag,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  child: TimerWidget(callback: () {}),
-                )),
-            SizedBox(
-              height: 8,
-            ),
-            Center(
-              child: TextField(
-                textAlign: TextAlign.center,
-                keyboardType: TextInputType.text,
-                maxLines: 1,
-                minLines: 1,
-                style: TextStyle(fontSize: 20),
-                decoration: InputDecoration(
-                  hintText: "제목을 적어주세요.",
-                  border: UnderlineInputBorder(borderSide: BorderSide.none),
-                  enabledBorder:
-                      UnderlineInputBorder(borderSide: BorderSide.none),
-                  focusedBorder:
-                      UnderlineInputBorder(borderSide: BorderSide.none),
+    final timerController = Get.put(TimerController());
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Hero(
+                      tag: k_timer_herotag,
+                      child: TimerWidget(callback: () async {
+                        Get.dialog(!Platform.isIOS
+                            ? await showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                  title: Text(k_alert_title),
+                                  content: Text(k_alert_content),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      child: Text(k_alert_yes),
+                                      onPressed: () =>
+                                          Get.offAll(MainPeedScreen()),
+                                    ),
+                                    TextButton(
+                                      child: Text(k_alert_no),
+                                      onPressed: () => Get.back(),
+                                    ),
+                                    TextButton(
+                                        child: Text(k_alert_cancle),
+                                        onPressed: () {
+                                          timerController.resetTimer();
+                                          Get.offAll(MainPeedScreen());
+                                        }),
+                                  ],
+                                ),
+                              )
+
+                            // todo : showDialog for ios
+                            : await showCupertinoDialog(
+                                context: context,
+                                builder: (context) => CupertinoAlertDialog(
+                                  title: Text(k_alert_title),
+                                  content: Text(k_alert_content),
+                                  actions: <Widget>[
+                                    CupertinoDialogAction(
+                                      child: Text(k_alert_yes),
+                                      onPressed: () =>
+                                          Get.offAll(MainPeedScreen()),
+                                    ),
+                                    CupertinoDialogAction(
+                                      child: Text(k_alert_no),
+                                      onPressed: () => Get.back(),
+                                    ),
+                                    CupertinoDialogAction(
+                                        child: Text(k_alert_cancle),
+                                        onPressed: () {
+                                          timerController.resetTimer();
+                                          Get.offAll(MainPeedScreen());
+                                        }),
+                                  ],
+                                ),
+                              ));
+                      })),
                 ),
-              ),
+                const Center(
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.text,
+                    maxLines: 1,
+                    minLines: 1,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                      hintText: "제목을 적어주세요.",
+                      border: UnderlineInputBorder(borderSide: BorderSide.none),
+                      enabledBorder:
+                          UnderlineInputBorder(borderSide: BorderSide.none),
+                      focusedBorder:
+                          UnderlineInputBorder(borderSide: BorderSide.none),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+                  child: TextField(
+                      keyboardType: TextInputType.multiline,
+                      minLines: 7, //Normal textInputField will be displayed
+                      maxLines: 7,
+                      style: TextStyle(fontSize: 18),
+                      decoration: InputDecoration(
+                        hintText: "15분간 일기를 써 주세요.",
+                        border:
+                            UnderlineInputBorder(borderSide: BorderSide.none),
+                        enabledBorder:
+                            UnderlineInputBorder(borderSide: BorderSide.none),
+                        focusedBorder:
+                            UnderlineInputBorder(borderSide: BorderSide.none),
+                      )),
+                )
+              ],
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 4, horizontal: 12),
-              child: TextField(
-                  keyboardType: TextInputType.multiline,
-                  minLines: 7, //Normal textInputField will be displayed
-                  maxLines: 7,
-                  style: TextStyle(fontSize: 18),
-                  decoration: InputDecoration(
-                    hintText: "15분간 일기를 써 주세요.",
-                    border: UnderlineInputBorder(borderSide: BorderSide.none),
-                    enabledBorder:
-                        UnderlineInputBorder(borderSide: BorderSide.none),
-                    focusedBorder:
-                        UnderlineInputBorder(borderSide: BorderSide.none),
-                  )),
-            )
-          ],
+          ),
         ),
       ),
     );
