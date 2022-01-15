6950e630154f3a3cc289b6fbe90c7abec95c7cc2
CodeWave <jamg123123@naver.com>
2022-01-15 15:41:06 +0900

    Feat: 업로드 누를 때 인디케이터 동작하게 리팩토링 하기
    - 데이터 업로드시 완료 될 때까지 인디케이터 표시
    - 토스트 메세지를 이용해서 업로드 내용 안내

--- lib/main_peed_screen/component/drawer_login_widget.dart
+++ lib/main_peed_screen/component/drawer_login_widget.dart
@@ -1,9 +1,11 @@
+import 'package:fifteen_minute_diary/controller/drawer_controller.dart';
 import 'package:fifteen_minute_diary/controller/post_controller.dart';
 import 'package:fifteen_minute_diary/custom_class/firebase_service.dart';
 import 'package:fifteen_minute_diary/custom_class/post.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 
 class DrawerLoginWidget extends StatelessWidget {
@@ -40,9 +42,14 @@ class DrawerLoginWidget extends StatelessWidget {
                 MaterialButton(
                   onPressed: () async {
                     print('업로드 버튼 입력');
+                    _show1MinuteRemainToast();
+                    Get.find<CustomDrawerController>().setIsShowIndicator(true);
                     Map<String, dynamic> list =
                         await Get.find<PostController>().getPostlistJson();
                     firebaseService.uploadDateToFireStore(list);
+
+                    Get.find<CustomDrawerController>()
+                        .setIsShowIndicator(false);
                   },
                   child: Text('데이터 업로드'),
                 ),
@@ -64,6 +71,18 @@ class DrawerLoginWidget extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _show1MinuteRemainToast() {
+    debugPrint('show Toast Message');
+    Fluttertoast.showToast(
+        msg: "데이터를 업로드 합니다.",
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.grey.shade800,
+        textColor: Colors.white,
+        fontSize: 16.0);
   }
 }
 
