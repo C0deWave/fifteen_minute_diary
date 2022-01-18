09f9f618d522bba70515ccc7f777125c74088b23
CodeWave <jamg123123@naver.com>
2022-01-18 15:21:39 +0900

    Refactor: 글쓰기 화면에서 여러 이미지 표시

--- lib/write_diary_screen/component/image_widget.dart
+++ lib/write_diary_screen/component/image_widget.dart
@@ -12,53 +12,40 @@ class ImageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<PostController>(builder: (controller) {
-      List<Widget> rowlist2 = List<Widget>.generate(
-          Get.find<PostController>().getPostlist()[0].imagelist!.length,
-          (index) => Text('${index}'));
-      List<Widget> rowlist = [
-        GestureDetector(
-          onTap: () {
-            debugPrint(_tag + "tab");
-            Get.dialog(const PhotoDialogWidget());
-          },
-          child: Container(
-            width: 200,
-            height: 200,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: FileImage(
-                        File(controller.getSelectedImageList()![0].path))),
-                borderRadius: BorderRadius.circular(15)),
-          ),
-        ),
-      ];
-
       return controller.getIsUsedImage()
           ? GetBuilder<PostController>(builder: (controller) {
-              return SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                    children: List<Widget>.generate(
-                        controller.getSelectedImageList()!.length,
-                        (index) => GestureDetector(
-                              onTap: () {
-                                debugPrint(_tag + "tab");
-                                Get.dialog(const PhotoDialogWidget());
-                              },
-                              child: Container(
-                                width: 150,
-                                height: 150,
-                                decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                        fit: BoxFit.fill,
-                                        image: FileImage(File(controller
-                                            .getSelectedImageList()![index]
-                                            .path))),
-                                    borderRadius: BorderRadius.circular(15)),
-                              ),
-                            ))),
-              );
+              if (controller.getSelectedImageList() != null) {
+                return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                      children: List<Widget>.generate(
+                          controller.getSelectedImageList()!.length,
+                          (index) => GestureDetector(
+                                onTap: () {
+                                  debugPrint(_tag + "tab");
+                                  Get.dialog(const PhotoDialogWidget());
+                                },
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(horizontal: 2),
+                                  child: Container(
+                                    width: 150,
+                                    height: 150,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            fit: BoxFit.fill,
+                                            image: FileImage(File(controller
+                                                .getSelectedImageList()![index]
+                                                .path))),
+                                        borderRadius:
+                                            BorderRadius.circular(15)),
+                                  ),
+                                ),
+                              ))),
+                );
+              } else {
+                return Container();
+              }
             })
           : Container();
     });
