5a754afb2cd560068f6c9fd3776aba49458b04e1
CodeWave <jamg123123@naver.com>
2022-01-19 13:55:02 +0900

    Feat: 대표이미지 + 이미지 개별 삭제 기능 추가
    - PostController에서 담당

--- lib/write_diary_screen/component/image_widget.dart
+++ lib/write_diary_screen/component/image_widget.dart
@@ -1,8 +1,7 @@
 import 'dart:io';
 import 'package:fifteen_minute_diary/controller/post_controller.dart';
-import 'package:fifteen_minute_diary/write_diary_screen/component/photo_dialog_widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 
 class ImageWidget extends StatelessWidget {
@@ -23,22 +22,70 @@ class ImageWidget extends StatelessWidget {
                           (index) => GestureDetector(
                                 onTap: () {
                                   debugPrint(_tag + "tab");
-                                  Get.dialog(const PhotoDialogWidget());
+                                  showCupertinoModalPopup<void>(
+                                      context: context,
+                                      builder: (BuildContext context) =>
+                                          CupertinoActionSheet(
+                                            title: const Text('사진 선택'),
+                                            // message: const Text('사진은 최대 5장까지 가능합니다.'),
+                                            actions: <
+                                                CupertinoActionSheetAction>[
+                                              CupertinoActionSheetAction(
+                                                child: const Text('대표 이미지로 지정'),
+                                                onPressed: () async {
+                                                  Get.find<PostController>()
+                                                      .setRepresentativeImage(
+                                                          index);
+                                                  Navigator.pop(context);
+                                                },
+                                              ),
+                                              CupertinoActionSheetAction(
+                                                child: const Text(
+                                                  '이미지 삭제',
+                                                  style: TextStyle(
+                                                      color: Colors.red),
+                                                ),
+                                                onPressed: () async {
+                                                  Get.find<PostController>()
+                                                      .removeImageOfIndex(
+                                                          index);
+                                                  Navigator.pop(context);
+                                                },
+                                              )
+                                            ],
+                                          ));
                                 },
                                 child: Padding(
                                   padding:
                                       const EdgeInsets.symmetric(horizontal: 2),
-                                  child: Container(
-                                    width: 150,
-                                    height: 150,
-                                    decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                            fit: BoxFit.fill,
-                                            image: FileImage(File(controller
-                                                .getSelectedImageList()![index]
-                                                .path))),
-                                        borderRadius:
-                                            BorderRadius.circular(15)),
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        width: 150,
+                                        height: 150,
+                                        decoration: BoxDecoration(
+                                            image: DecorationImage(
+                                                fit: BoxFit.contain,
+                                                image: FileImage(File(controller
+                                                    .getSelectedImageList()![
+                                                        index]
+                                                    .path))),
+                                            borderRadius:
+                                                BorderRadius.circular(15),
+                                            border: index == 0
+                                                ? Border.all(
+                                                    width: 2,
+                                                    color: Colors.orange)
+                                                : Border.all(
+                                                    width: 0.2,
+                                                    color: Colors.black)),
+                                      ),
+                                      Text(
+                                        index == 0 ? "대표이미지" : "",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
                                   ),
                                 ),
                               ))),
