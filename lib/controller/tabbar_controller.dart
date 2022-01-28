dfb424e59f95010c774db60b8a0e74750f4a4e02
CodeWave <jamg123123@naver.com>
2022-01-28 15:41:25 +0900

    Refactor: Tabbar Stateful 에서 stateless로 변경
    - 상태관리를 GetX에서 맡게 변경

--- lib/controller/tabbar_controller.dart
+++ lib/controller/tabbar_controller.dart
@@ -4,6 +4,7 @@ enum TabbarState { diaryState, calendarState }
 
 class TabbarController extends GetxController {
   TabbarState currentState = TabbarState.diaryState;
+  int index = 0;
 
   void changeTabbarState(TabbarState state) {
     currentState = state;
--- lib/main_peed_screen/component/tabbar_widget.dart
+++ lib/main_peed_screen/component/tabbar_widget.dart
@@ -3,64 +3,57 @@ import 'package:fifteen_minute_diary/controller/tabbar_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class TabbarWidget extends StatefulWidget {
-  const TabbarWidget({Key? key}) : super(key: key);
-
-  @override
-  State<TabbarWidget> createState() => _TabbarWidgetState();
-}
-
-class _TabbarWidgetState extends State<TabbarWidget> {
-  int index = 0;
+class TabbarWidget extends StatelessWidget {
+  // int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 0),
-      child: Container(
-        color: Colors.white,
-        child: CupertinoTabBar(
-          Colors.white12,
-          Color.fromARGB(255, 48, 119, 51),
-          [
-            Text(
-              "내 일기 목록",
-              style: TextStyle(
-                color: index == 0 ? Colors.white : Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
+      child: GetBuilder<TabbarController>(
+        builder: (tabbarController) => Container(
+          color: Colors.white,
+          child: CupertinoTabBar(
+            Colors.white12,
+            Color.fromARGB(255, 48, 119, 51),
+            [
+              Text(
+                "내 일기 목록",
+                style: TextStyle(
+                  color:
+                      tabbarController.index == 0 ? Colors.white : Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              "둘러보기",
-              style: TextStyle(
-                color: index == 1 ? Colors.white : Colors.black,
-                fontSize: 21,
-                fontWeight: FontWeight.w600,
+              Text(
+                "둘러보기",
+                style: TextStyle(
+                  color:
+                      tabbarController.index == 1 ? Colors.white : Colors.black,
+                  fontSize: 21,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
-            ),
-          ],
-          () {
-            return index;
-          },
-          (data) {
-            if (data == 0) {
-              Get.find<TabbarController>()
-                  .changeTabbarState(TabbarState.diaryState);
-            } else {
-              Get.find<TabbarController>()
-                  .changeTabbarState(TabbarState.calendarState);
-            }
-            setState(() {
-              index = data;
-            });
-          },
-          allowExpand: true,
-          useShadow: false,
-          innerVerticalPadding: 10,
-          outerHorizontalPadding: 30,
+            ],
+            () {
+              return tabbarController.index;
+            },
+            (data) {
+              if (data == 0) {
+                tabbarController.changeTabbarState(TabbarState.diaryState);
+              } else {
+                tabbarController.changeTabbarState(TabbarState.calendarState);
+              }
+              tabbarController.index = data;
+            },
+            allowExpand: true,
+            useShadow: false,
+            innerVerticalPadding: 10,
+            outerHorizontalPadding: 30,
+          ),
         ),
       ),
     );
